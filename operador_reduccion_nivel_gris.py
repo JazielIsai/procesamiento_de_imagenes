6df36554620901dbda12c888e333dff681970a79
Jaziel Isaí <92839539+JazielIsai@users.noreply.github.com>
2022-07-12 15:03:08 -0500

    Add files via upload

--- operador_reduccion_nivel_gris.py
+++ operador_reduccion_nivel_gris.py
@@ -1,18 +1,21 @@
 from PIL import Image
 
 # En esta operación lo que se busca es reducir los niveles grises que tiene una imagen,
-# como sabemos inicialmente tenemos 256 niveles de gris (0-255), e
-# ntonces esta operación nos permite definir
+# como sabemos inicialmente tenemos 256 niveles de gris (0-255),
+# entonces esta operación nos permite definir
 # que cantidad de niveles de gris queremos tener en nuestra imagen resultante.
 #
 # 10.- Operador reducción nivel gris
 #
 #     q = 0 para p <= p1
-#     -q1 = para p1 < p <= p2
-#     q2 = para p2 < p <= p3
+#     q = q1 para p1 < p <= p2
+#     q = q2 para p2 < p <= p3
+#     .
 #     .
 #     .
 #     qn para pn-1 < p <= 255
+
+umbral = int(input("El umbral es de: \t"))
 
 img = Image.open('perfil-en-blanco-y-negro.jpg')
 img_gray = img.convert('L')
@@ -23,5 +26,13 @@ print(img_pixels)
 
 lst = []
 
+for i in img_pixels:
+    if i <= umbral:
+        lst.append(0)
+    else:
+        res = (umbral * 2) / 2
+        lst.append(res)
 
-
+img_new = Image.new('L', img_gray.size)
+img_new.putdata(lst)
+img_new.show()
